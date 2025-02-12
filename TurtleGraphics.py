f0f47364628f3ff74354c7b31409037e1739f0ce
BMW060 <brennan.m.wood@gmail.com>
2025-02-12 16:25:09 -0600

    Almost there

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -6,14 +6,66 @@
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
 
-def drawSquare(myTurtle, size):
+# Drawing a square
+def drawSquare(kili, size):
     for i in range(4):
-        myTurtle.forward(size)
-        myTurtle.right(90)
-
+        kili.forward(size)
+        kili.right(90)
+        
+# Drawing a polygon
+def drawPolygon(kili, sides):
+    for s in range(sides):
+        kili.forward(100)
+        kili.right(360/sides)
+        
+        
+# Defining a filled square
+def fillSquare(kili):
+    kili.begin_fill()
+    drawSquare(kili, 50)
+    kili.end_fill()
+    
+    
+# Filled square function    
+def fillCorner(kili, corner):
+    drawSquare(kili, 100)
+    if corner == 1:
+        fillSquare(kili)
+    if corner == 2:
+        kili.up()
+        kili.forward(50)
+        kili.down()
+        fillSquare(kili)
+    if corner == 3:
+        kili.up()
+        kili.right(90)
+        kili.forward(50)
+        kili.left(90)
+        kili.down()
+        fillSquare(kili)
+    if corner == 4:
+        kili.up()
+        kili.forward(50)
+        kili.right(90)
+        kili.forward(50)
+        kili.left(90)
+        kili.down()
+        fillSquare(kili)
+        
+        
+# Squares in squares
+def squaresInSquares(kili, squares):
+    
+    
+    
 
 def main():
-    myTurtle = turtle.Turtle()
+    kili = turtle.Turtle()
+    
+    fillCorner(kili,4)
+    
+    # drawPolygon(kili,8)
+    
     # drawPolygon(myTurtle, 5) #draws a pentagon
     # drawPolygon(myTurtle, 8) #draws an octogon
 
